01c24de3774e8c434e89d81fa4b92c8256451bdd
seangchae <ch.beak1024@gmail.com>
2025-03-08 14:54:46 +0900

    Add files via upload

---
+++ 2025-3-8.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	printf("My name is seangchae.\n");
+	
+	printf("Im from cheong myeong highschool.\n");
+	
+	printf("Im 17 years old.\n");
+	
+	return 0;
+	*//*
+	printf("Hello\nC3coding\n");
+	printf("Hello\bC3coding\n");
+	printf("Hello\rC3coding\n");
+	printf("Hello\tC3coding\n");
+	*//*
+	printf("12345678\n");
+	printf("ABCDEFG\n");
+	printf("¥Î«—πŒ±π\n");
+	printf("abcdefg\n");
+	printf("°›°ﬂ¢¿¢æ");
+	printf("!@#$%^&*\n");
+	printf("12345678\n");
+	*/
+	printf("%%Solar Tracking System%%\n");
+	printf("\\Smart Devices\\\n");
+	printf("\"Matrix\"\n");
+	printf("\'Hello Robot\'\n");
+}
+
